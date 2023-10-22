e55e6a35da3fd957c7cac6af598d55f0f0a68d9e
zahrabateninia <zahrabatenin@gmail.com>
2023-10-22 09:42:36 +0330

    Connect the socket to Google's server on port 80

--- connectToGoogle.py
+++ connectToGoogle.py
@@ -1,5 +1,5 @@
 import socket
-
+import sys
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,3 +8,16 @@ try:
 except socket.error as err:
     print("Socket creation failed with error %s" %(err))
 
+port = 80 # default port for HTTP communication
+
+try:
+    host_ip = socket.gethostbyname('www.google.com')
+
+except socket.gaierror: # Get Address Info Error
+    print("There was an error resolving the host")
+    sys.exit()
+
+# connecting to the server
+s.connect((host_ip,port))
+
+print("The socket has successfully connected to Google")
