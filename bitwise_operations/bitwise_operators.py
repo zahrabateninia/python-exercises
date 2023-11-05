1259583d330459a990c2f74362f2b6b7b6bcc10a
zahrabateninia <zahrabatenin@gmail.com>
2023-11-05 13:50:34 +0330

    Add XOR operator

--- bitwise_operations/bitwise_operators.py
+++ bitwise_operations/bitwise_operators.py
@@ -10,3 +10,6 @@ print(f"AND is: {bin(n3)[2:]}")
 # OR
 n4 = n1 | n2
 print(f"OR is: {bin(n4)[2:]}")
+# XOR (if the two values are different, we get 1) -> 1 XOR 0 is 1
+n5 = n1 ^ n2
+print(f"XOR is: 0{bin(n5)[2:]}")
