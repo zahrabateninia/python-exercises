719df8761046654145cdce842fb9af74cbf597e6
zahrabateninia <zahrabatenin@gmail.com>
2023-11-05 14:40:14 +0330

    Shift the number to the left one time

--- bitwise_operations/bitwise_operators.py
+++ bitwise_operations/bitwise_operators.py
@@ -13,7 +13,12 @@ print(f"OR: {bin(n4)[2:]}")
 # XOR (if the two values are different, we get 1) -> 1 XOR 0 is 1
 n5 = n1 ^ n2
 print(f"XOR: 0{bin(n5)[2:]}")
-#NOT (negation operator: it does not turn all the bits around because we're working with unsigned integers.) 
+# NOT (negation operator: it does not turn all the bits around because we're working with unsigned integers.) 
 print(f"NOT: {bin(~n1)[3:]}")
 negation_of_n1 = bin(0b111111111111111 - n1)[2:]
 print(f"Exact negation of n1: 0{negation_of_n1}")
+
+# Shifts
+number = 20
+number <<=1
+print(number)
