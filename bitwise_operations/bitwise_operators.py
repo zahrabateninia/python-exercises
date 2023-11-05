e55ebaeffd389a5f1f74c86d22ffed2a1f1ca589
zahrabateninia <zahrabatenin@gmail.com>
2023-11-05 14:02:44 +0330

    Add NOT operator

--- bitwise_operations/bitwise_operators.py
+++ bitwise_operations/bitwise_operators.py
@@ -1,15 +1,17 @@
 n1 = 23477
 n2 = 31213
 
-print(bin(n1)[2:])
-print(bin(n2)[2:])
+print(f"n1 = {bin(n1)[2:]}")
+print(f"n1 = {bin(n2)[2:]}")
 
 # AND
 n3 = n1 & n2
-print(f"AND is: {bin(n3)[2:]}")
+print(f"AND: {bin(n3)[2:]}")
 # OR
 n4 = n1 | n2
-print(f"OR is: {bin(n4)[2:]}")
+print(f"OR: {bin(n4)[2:]}")
 # XOR (if the two values are different, we get 1) -> 1 XOR 0 is 1
 n5 = n1 ^ n2
-print(f"XOR is: 0{bin(n5)[2:]}")
+print(f"XOR: 0{bin(n5)[2:]}")
+#NOT (negation operator: it does not turn all the bits around because we're working with unsigned integers.) 
+print(f"NOT: {bin(~n1)[3:]}")
